e22b6f2fa2f7e6cb29fea30d645e2e311d0d838d
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2018-05-18 15:39:30 +0200

    users CRUD

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -4,7 +4,7 @@ var jwt = require('jsonwebtoken');
 
 
 //get specific object
-router.post('/connection', function(req, res, next) {
+router.post('/connexion', function(req, res, next) {
 	connection.query('SELECT * from user WHERE email=? and password=?',[req.body.email,req.body.password], function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
@@ -60,8 +60,70 @@ router.use(function(req, res, next) {
   }
 });
 
+//get all users
+router.get('/', function(req, res, next) {
+	connection.query('SELECT * from user', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
 
-// apply the routes to our application with the prefix /api
-//app.use('/api', apiRoutes);
+//get specific object
+router.get('/:user_id', function(req, res, next) {
+	connection.query('SELECT * from user WHERE id = ' + req.params.user_id, function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+//modify object
+router.patch('/:user_id', function(req, res, next) {
+	connection.query('UPDATE user SET email = ' + req.body.email + ', nom = ' + req.body.nom + ', prenom = "' + req.body.prenom +'", password = ' + req.body.password + ', role = ' + req.body.role + ', siteEPF = ' + req.body.siteEPF + ' WHERE id = ' + req.params.user_id, function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+//delete object
+router.delete('/:user_id', function(req, res, next) {
+	connection.query('DELETE FROM user WHERE id = ' + req.params.user_id, function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+//create object
+router.post('/', function(req, res, next) {
+	connection.query('INSERT INTO user (email, nom, prenom, password, role, siteEPF) VALUES (' + + req.body.email + ',' + req.body.nom + ',"' + req.body.prenom +'",' + req.body.password + ',' + req.body.role + ',' + req.body.siteEPF +')', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
 
 module.exports = router;
