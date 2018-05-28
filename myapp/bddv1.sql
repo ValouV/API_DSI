c46551984c4c62e4ae2ca252f28fa6bb2f5c0d4b
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2018-05-28 22:22:01 +0200

    update bdd

--- myapp/bddv1.sql
+++ myapp/bddv1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le :  mer. 23 mai 2018 à 16:06
+-- Généré le :  lun. 28 mai 2018 à 22:21
 -- Version du serveur :  5.6.38
 -- Version de PHP :  7.0.12
 
@@ -29,6 +29,14 @@ CREATE TABLE `alertepret` (
   `idHistoriquePret` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `alertepret`
+--
+
+INSERT INTO `alertepret` (`id`, `date`, `message`, `lu`, `type`, `idHistoriquePret`) VALUES
+(1, '2018-05-18 00:00:00', 'Test d\'alerte', 1, 1, 2),
+(2, '2018-05-28 00:00:00', 'Test LAurène tes grands morts', 1, 1, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +51,13 @@ CREATE TABLE `alertestock` (
   `type` int(11) NOT NULL,
   `idHistoriqueStock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `alertestock`
+--
+
+INSERT INTO `alertestock` (`id`, `date`, `message`, `lu`, `type`, `idHistoriqueStock`) VALUES
+(1, '2018-05-28 00:00:00', 'Test alerte stock', 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -62,7 +77,7 @@ CREATE TABLE `categorie` (
 INSERT INTO `categorie` (`id`, `nom`) VALUES
 (2, 'écran 25 pouces'),
 (3, 'écran 25 pouces'),
-(10, 'cuillère');
+(4, 'cuillère');
 
 -- --------------------------------------------------------
 
@@ -82,21 +97,8 @@ CREATE TABLE `catlimite` (
 --
 
 INSERT INTO `catlimite` (`id`, `limite`, `idCategorie`, `siteEPF`) VALUES
-(2, 10, 2, 2),
+(2, 20, 2, 2),
 (3, 10, 3, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `helisa`
---
-
-CREATE TABLE `helisa` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -119,7 +121,8 @@ CREATE TABLE `historiquepret` (
 --
 
 INSERT INTO `historiquepret` (`id`, `depart`, `retourPrevu`, `retourEffectif`, `idUserAdmin`, `idObjet`, `idUserHelisa`) VALUES
-(2, '2018-05-20 20:00:00', '2018-05-21 20:00:00', '0000-00-00 00:00:00', 1, 2, 1);
+(2, '2018-05-20 20:00:00', '2018-05-21 20:00:00', '0000-00-00 00:00:00', 1, 2, 1),
+(3, '2018-05-02 00:00:00', '2018-05-03 00:00:00', '2018-05-04 00:00:00', 3, 10004, 1);
 
 -- --------------------------------------------------------
 
@@ -140,13 +143,17 @@ CREATE TABLE `historiquestock` (
 --
 
 INSERT INTO `historiquestock` (`id`, `arrivée`, `depart`, `idUserAdmin`, `idObjet`) VALUES
-(2, '2018-05-21 21:00:00', '2018-05-21 21:50:00', 1, 1),
+(2, '2018-05-21 21:00:00', '2018-05-21 21:50:00', 2, 2),
 (3, '2018-05-21 21:00:00', '0000-00-00 00:00:00', 1, 1),
 (4, '2018-05-21 21:00:00', '0000-00-00 00:00:00', 1, 1),
 (5, '2018-05-21 21:00:00', '0000-00-00 00:00:00', 1, 1),
 (6, '2018-05-21 21:00:00', '0000-00-00 00:00:00', 1, 1),
 (7, '2018-05-21 21:00:00', '0000-00-00 00:00:00', 1, 1),
-(8, '2018-05-21 21:00:00', '0000-00-00 00:00:00', 1, 1);
+(8, '2018-05-21 21:00:00', '0000-00-00 00:00:00', 1, 1),
+(9, '0000-00-00 00:00:00', '2018-05-28 19:32:38', 3, 2),
+(10, '0000-00-00 00:00:00', '2018-05-28 19:33:55', 3, 2),
+(11, '2018-05-28 19:35:08', '2018-05-28 19:35:45', 3, 2),
+(12, '2018-05-28 19:36:51', '0000-00-00 00:00:00', 3, 2);
 
 -- --------------------------------------------------------
 
@@ -169,11 +176,48 @@ CREATE TABLE `objet` (
 --
 
 INSERT INTO `objet` (`id`, `actif`, `isStock`, `commentaire`, `siteEPF`, `idCategorie`, `idUser`) VALUES
-(2, 1, 0, 'Test API tes grands morts Victor', 1, 2, 1),
+(2, 1, 0, 'ça a marché batard', 1, 1, 3),
 (10000, 1, 1, 'yo', 1, 10, 10),
 (10001, 4, 1, 'ah', 4, 4, 1),
 (10002, 4, 1, 'ah', 4, 4, 1),
-(10003, 0, 0, 'Nouvel objet numéro 353', 1, 1, 1);
+(10003, 0, 0, 'Nouvel objet numéro 353', 1, 1, 1),
+(10004, 1, 0, 'Test ajout', 1, 4, 3),
+(10005, 1, 0, 'Test ajout', 1, 4, 3),
+(10006, 1, 0, 'Test ajout', 1, 4, 3),
+(10007, 1, 0, 'Test ajout', 1, 4, 3),
+(10008, 1, 0, 'Test ajout', 1, 4, 3),
+(10009, 1, 0, 'Test ajout', 1, 4, 3),
+(10010, 1, 0, 'Test ajout', 1, 4, 3),
+(10011, 1, 0, 'Test ajout', 1, 1, 3),
+(10012, 1, 0, 'Test ajout', 1, 2, 3),
+(10013, 1, 0, 'Test ajout', 1, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `uHelisa`
+--
+
+CREATE TABLE `uHelisa` (
+  `ID_ETUDIANT` varchar(255) NOT NULL,
+  `APPRENANT_PRENOM` varchar(255) NOT NULL,
+  `APPRENANT_NOM` varchar(255) NOT NULL,
+  `PHOTO_IDENTITE` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `uHelisa`
+--
+
+INSERT INTO `uHelisa` (`ID_ETUDIANT`, `APPRENANT_PRENOM`, `APPRENANT_NOM`, `PHOTO_IDENTITE`, `EMAIL`) VALUES
+('E03879', 'AGUER', 'Victor', 'aguervictor.jpg', 'victor.aguer@epfedu.fr'),
+('E04493', 'BERNADET', 'David', 'bernadetdavid.jpg', 'david.bernadet@epfedu.fr'),
+('E04512', 'DANTY', 'Alexandre', 'dantyalexandre.jpg', 'alexandre.danty@epfedu.fr'),
+('E04770', 'DEGUERRE', 'Lancelot', 'deguerrelancelot.jpg', 'lancelot.deguerre@epfedu.fr'),
+('E04224', 'RIOU', 'Louis', 'rioulouis.jpg', 'louis.riou@epfedu.fr'),
+('E04804', 'VASSAS', 'Valentin', 'vassasvalentin.jpg', 'valentin.vassas@epfedu.fr'),
+('E04731', 'YVIQUEL', 'Laurène', 'yviquellaurene.jpg', 'laurene.yviquel@epfedu.fr');
 
 -- --------------------------------------------------------
 
@@ -198,7 +242,20 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `email`, `nom`, `prenom`, `password`, `role`, `siteEPF`) VALUES
 (1, 'email', 'nom1', 'prenom1', '', 0, 0),
 (2, 'email2', 'nom2', 'prenom2', '', 1, 0),
-(3, 'admin', 'admin', 'admin', 'admin', 1, 1);
+(3, 'admin', 'admin', 'admin', 'admin', 1, 1),
+(4, 'test@test', 'test', 'test', 'test', 1, 1),
+(5, 'test@test', 'test', 'test', 'test', 1, 1),
+(6, 'test@test', 'test', 'test', 'test', 1, 1),
+(7, 'test@test', 'test', 'test', 'test', 1, 1),
+(8, 'test@test', 'test', 'test', 'test', 1, 1),
+(9, 'test@test', 'test', 'test', 'test', 1, 1),
+(10, 'test@test', 'test', 'test', 'test', 1, 1),
+(11, 'test@test', 'test', 'test', 'test', 1, 1),
+(12, 'tes', 'test', 'test', 'test', 1, 1),
+(13, 'tes', 'test', 'test', 'test', 1, 1),
+(14, 'tes', 'test', 'test', 'test', 1, 1),
+(15, 'tes', 'test', 'test', 'test', 1, 1),
+(16, 'test@', 'test', 'test', 'test', 1, 1);
 
 --
 -- Index pour les tables déchargées
@@ -226,12 +283,6 @@ ALTER TABLE `categorie`
 -- Index pour la table `catlimite`
 --
 ALTER TABLE `catlimite`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `helisa`
---
-ALTER TABLE `helisa`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -266,19 +317,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `alertepret`
 --
 ALTER TABLE `alertepret`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `alertestock`
 --
 ALTER TABLE `alertestock`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `catlimite`
@@ -287,31 +338,25 @@ ALTER TABLE `catlimite`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT pour la table `helisa`
---
-ALTER TABLE `helisa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT pour la table `historiquepret`
 --
 ALTER TABLE `historiquepret`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `historiquestock`
 --
 ALTER TABLE `historiquestock`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `objet`
 --
 ALTER TABLE `objet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10004;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10014;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
